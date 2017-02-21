37eb81cc8482541f9bfee543c8e15dd43974094c
edgarOPG <eopg95@gmail.com>
2017-02-20 23:03:06 -0700

    added handlebars example

---
+++ server.js
@@ -0,0 +1,19 @@
+//declaramos modo estricto
+'use strict';
+
+//Includes
+var express = require('express'),
+    exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000, function () {
+    console.log('express-handlebars example listening on: 3000');
+});
